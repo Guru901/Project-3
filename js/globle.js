cba540416329848faad27fdb4a8dc3f7d05aaf2c
Guru901 <133991448+Guru901@users.noreply.github.com>
2024-01-24 11:09:30 +0000

    Nostalgia of writing bad code

--- js/globle.js
+++ js/globle.js
@@ -1,13 +1,26 @@
 Shery.mouseFollower();
 
 Shery.imageMasker(".hvr-img", {
-    mouseFollower: true,
-    text: "Buy Now",
-    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
-    duration: 1,
-    zoom: 1.5,
+  mouseFollower: true,
+  text: "Buy Now",
+  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  duration: 1,
+  zoom: 1.5,
 });
 
 Shery.makeMagnet(".mgn", {
-    duration: 1,
+  duration: 1,
 });
+
+const lenis = new Lenis();
+
+lenis.on("scroll", (e) => {
+  console.log(e);
+});
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
